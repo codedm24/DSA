51c098ce87baad0a0122542d01036af5b5922ab0
codedm24 <mithun.adroit@gmail.com>
2025-02-12 17:58:09 +0530

    BubbleSort, SelectionSort

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,12 +1,31 @@
+#Bubble sort
+run_code = 1
 array1 = [64,34,25,12,22,11,90]
 
 n = len(array1)
 
-for i in range(n-1):
-    print(f"i: {i}")
-    for j in range(n-i-1):
-        print(f"j: {j}")
-        if(array1[j] > array1[j+1]):
-            array1[j], array1[j+1] = array1[j+1], array1[j]
+if run_code == 1:
 
-print(f"Sorted array: {array1}")
+    for i in range(n-1):
+        print(f"i: {i}")
+        for j in range(n-i-1):
+            print(f"j: {j}")
+            if(array1[j] > array1[j+1]):
+                array1[j], array1[j+1] = array1[j+1], array1[j]
+
+    print(f"Sorted array: {array1}")
+
+elif run_code == 2:
+    
+    #Bubble sort improvement
+    for i in range(n-1):
+        #print(f"i: {i}")
+        swapped = False
+        for j in range(n-i-1):
+            print(f"j: {j}")
+            if(array1[j] > array1[j+1]):
+                array1[j], array1[j+1] = array1[j+1], array1[j]
+                swapped = True
+        if not swapped:
+            break
+    print(f"Sorted array: {array1}")
---
+++ SelectionSort.py
@@ -0,0 +1,30 @@
+#Selection sort
+
+array1 = [64,34,25,5,22,11,90,12]
+
+n = len(array1)
+
+code_run = 2
+
+if code_run == 1:
+
+    for i in range(n-1):
+        min_index = i
+        for j in range(i+1,n):
+            if(array1[j] < array1[min_index]):
+                min_index = j
+        min_value = array1.pop(min_index)
+        array1.insert(i,min_value)
+
+    print("Sorted array {}".format(array1))
+
+elif code_run == 2:
+    
+    for i in range(n-1):
+        min_index = i
+        for j in range(i+1,n):
+            if(array1[j] < array1[min_index]):
+                min_index = j
+        array1[i], array1[min_index] = array1[min_index], array1[i]
+
+    print("Sorted array {}".format(array1))
